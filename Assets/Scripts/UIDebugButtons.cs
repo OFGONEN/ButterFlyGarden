b471bbe357ff70d6993c70e2d046bb61b42b03aa
OFGONEN <ofgonen@gmail.com>
2020-12-20 14:52:07 +0300

    Implement  Debug UI Buttons

---
+++ Assets/Scripts/UIDebugButtons.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class UIDebugButtons : MonoBehaviour
+{
+    GameObject water;
+    public void ToggleWater()
+    {
+        if (water == null)
+            water = GameObject.Find("Water");
+
+        if (water != null)
+        {
+            water.SetActive(!water.activeInHierarchy);
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        DOTween.KillAll();
+
+        var _level = PlayerPrefs.GetInt("Level");
+        PlayerPrefs.SetInt("Level", _level + 1);
+
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+    }
+
+    public void LoadPrevLevel()
+    {
+        DOTween.KillAll();
+
+        var _level = PlayerPrefs.GetInt("Level");
+        PlayerPrefs.SetInt("Level", _level - 1);
+
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+    }
+}
