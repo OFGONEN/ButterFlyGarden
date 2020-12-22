24f1629530f30e843781b957e5532464608a0b75
ofgonen <ofgonen@gmail.com>
2020-12-22 07:24:50 -0800

    Implement Facebook SDK

---
+++ Assets/Scripts/FFAnalitics.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Facebook.Unity;
+using NaughtyAttributes;
+
+public class FFAnalitics : MonoBehaviour
+{
+    private void Awake()
+    {
+        if (!FB.IsInitialized)
+            FB.Init(OnFacebookInitialized, OnHideUnity);
+        else
+            FB.ActivateApp();
+    }
+
+    void OnFacebookInitialized()
+    {
+        if (FB.IsInitialized)
+        {
+            FB.ActivateApp();
+            Debug.Log("[FFAnalitic] Facebook initiliazed");
+        }
+        else
+            Debug.Log("[FFAnalitic] Failed to initialize Facebook SDK");
+
+
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+    void OnHideUnity(bool hide)
+    {
+
+    }
+}
